f69f662c83d0010309848e7f50634356ff1be19d
радон <96480522+RadonRebirth@users.noreply.github.com>
2021-12-21 16:25:40 +0300

    Змейка
    змея ползает по консоли и кушает яблоки в c#.
    Для запуска перейдите в папку Game/bin/Debug
    И запустите файл Game.exe

---
+++ Game/Game/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using static System.Console;
+using System.Threading;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Game
+{
+    class Program
+    {
+        private const int MapWidth = 30;
+        private const int MapHeight = 20;
+
+        private const int ScreenWidth = MapWidth * 3;
+        private const int ScreenHeight = MapHeight * 3;
+
+        private const int FrameMs = 200;
+
+        private const ConsoleColor BorderColor = ConsoleColor.Blue;
+
+        private const ConsoleColor HeadColor = ConsoleColor.DarkMagenta;
+        private const ConsoleColor BodyColor = ConsoleColor.Green;
+
+        private const ConsoleColor FoodColor = ConsoleColor.Red;
+
+        private static readonly Random Random = new Random();
+
+        static void Main()
+        {
+            SetWindowSize(ScreenWidth, ScreenHeight);
+            SetBufferSize(ScreenWidth, ScreenHeight);
+            CursorVisible = false;
+
+            while(true)
+            {
+                StartGame();
+
+                Thread.Sleep(1000);
+                ReadKey();
+            }
+        }
+
+        static void StartGame()
+        {
+            Clear();
+
+            DrawBorder();
+
+            Direction currentMovement = Direction.Right;
+
+            var snake = new Snake(10, 5, HeadColor, BodyColor);
+
+            Pixel food = GenFood(snake);
+            food.Draw();
+
+            Stopwatch sw = new Stopwatch();
+
+            while (true)
+            {
+                sw.Restart();
+
+                Direction oldMovement = currentMovement;
+
+                while (sw.ElapsedMilliseconds <= FrameMs)
+                {
+                    if (currentMovement == oldMovement)
+                    {
+                        currentMovement = ReadMovement(currentMovement);
+                    }
+                }
+
+                if(snake.Head.X == food.X && snake.Head.Y == food.Y)
+                {
+                    snake.Move(currentMovement, true);
+                    food = GenFood(snake);
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move(currentMovement);
+                }
+
+
+                if (snake.Head.X == MapWidth - 1
+                    || snake.Head.X == 0
+                    || snake.Head.Y == MapHeight - 1
+                    || snake.Head.Y == 0
+                    || snake.Body.Any(b => b.X == snake.Head.X && b.Y == snake.Head.Y))
+                    break;
+
+            }
+
+            snake.Clear();
+
+            SetCursorPosition(ScreenWidth / 2, ScreenHeight / 2);
+            WriteLine("Game over");
+
+            ReadKey();
+            
+        }
+         
+        static Pixel GenFood(Snake snake)
+        {
+            Pixel food;
+
+            do
+            {
+                food = new Pixel(Random.Next(1, MapWidth - 2), Random.Next(1, MapHeight - 2), FoodColor);
+            } while (snake.Head.X == food.X && snake.Head.Y == food.Y
+                      || snake.Body.Any(b => b.X == food.X && b.Y == food.Y));
+            return food;
+        }
+
+        static  Direction ReadMovement(Direction currentDirection)
+        {
+            if (!KeyAvailable)
+                return currentDirection;
+
+            ConsoleKey key = ReadKey(true).Key;
+
+            currentDirection = key switch
+            {
+                ConsoleKey.UpArrow when currentDirection != Direction.Down => Direction.Up,
+                ConsoleKey.DownArrow when currentDirection != Direction.Up => Direction.Down,
+                ConsoleKey.LeftArrow when currentDirection != Direction.Right => Direction.Left,
+                ConsoleKey.RightArrow when currentDirection != Direction.Left => Direction.Right,
+                _ => currentDirection
+            };
+
+            return currentDirection;
+        }
+
+        static void DrawBorder()
+        {
+            for (int i = 0; i < MapWidth; i++)
+            {
+                new Pixel(i,0, BorderColor).Draw();
+                new Pixel(i,MapHeight - 1, BorderColor).Draw();
+            }
+
+            for (int i = 0; i < MapHeight; i++)
+            {
+                new Pixel(0, i, BorderColor).Draw();
+                new Pixel(MapWidth - 1, i, BorderColor).Draw();
+            }
+        }
+    }
+}
---
+++ Game/Game/Snake.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    public class Snake
+    {
+        private readonly ConsoleColor HeadColor;
+        private readonly ConsoleColor BodyColor;
+
+        public Snake(int initialX,
+            int initialY,
+            ConsoleColor headColor,
+            ConsoleColor bodyColor,
+            int bodylength = 3)
+        {
+            HeadColor = headColor;
+            BodyColor = bodyColor;
+
+            Head = new Pixel(initialX, initialY, HeadColor);
+
+            for (int i = bodylength; i >= 0; i--)
+            {
+                Body.Enqueue(new Pixel(Head.X - i - 1, initialY, BodyColor));
+            }
+            Draw();
+        }
+        public Pixel Head { get; private set; }
+
+        public Queue<Pixel> Body { get; } = new Queue<Pixel>();
+
+        public void Move(Direction direction, bool eat = false)
+        {
+            Clear();
+
+            Body.Enqueue(new Pixel(Head.X, Head.Y, BodyColor));
+
+            if (!eat)
+                Body.Dequeue();
+
+            Head = direction switch
+            {
+                Direction.Right => new Pixel(Head.X + 1, Head.Y, HeadColor),
+                Direction.Left => new Pixel(Head.X - 1, Head.Y, HeadColor),
+                Direction.Up => new Pixel(Head.X, Head.Y - 1, HeadColor),
+                Direction.Down => new Pixel(Head.X, Head.Y + 1, HeadColor),
+                _ => Head
+            };
+            Draw();
+        }
+
+        public void Draw()
+        {
+            Head.Draw();
+
+            foreach (Pixel pixel in Body)
+            {
+                pixel.Draw();
+            }
+        }
+
+        public void Clear()
+        {
+            Head.Clear();
+
+            foreach (Pixel pixel in Body)
+            {
+                pixel.Clear();
+            }
+        }
+    }
+}
